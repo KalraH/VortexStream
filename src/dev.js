509b7368c5c3e5baff8bf031afb928d86c328b40
Hariom Kalra <kalrahariom2001@gmail.com>
2025-08-04 14:45:24 +0530

    Deployment Bugs.

---
+++ src/dev.js
@@ -0,0 +1,13 @@
+import app from "./app.js";
+
+/* Listning to DB. */
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(
+		"DEV,",
+		`Local Server is running on port ${PORT}`
+	);
+});
+
+console.log("INDEX,", "Express app initialized successfully.");
